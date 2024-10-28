49da2e174c7ee25ee06143f8b6cd3d790319d835
MichalKalmucki <kalmuckimichal@gmail.com>
2024-10-28 10:17:32 +0100

    small refactor

---
+++ EDA/convert_to_csv.py
@@ -0,0 +1,12 @@
+from read_data import read_data_json, read_labels
+import pandas as pd
+
+
+def convert_to_csv(prefix=""):
+    users_df = read_data_json(prefix=prefix)
+    labels = read_labels(prefix=prefix)
+    print(users_df.head())
+    print(labels.head())
+
+    merged = pd.merge(users_df, labels, on="user_id")
+    merged.to_csv(f"{prefix}midterm-2018/data.csv")
--- EDA/read_data.py
+++ EDA/read_data.py
@@ -12,5 +12,13 @@ def read_data_json(prefix=""):
         return users_df
 
 
+def read_labels(prefix=""):
+    file_path = f"{prefix}midterm-2018/midterm-2018.tsv"
+
+    labels = pd.read_csv(file_path, sep="\t")
+    labels.columns = ["user_id", "label"]
+    return labels
+
+
 if __name__ == "__main__":
     read_data_json()
